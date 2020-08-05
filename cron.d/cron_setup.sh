1b2946148a6f473ec576e9824e1268b00890fa2b
Sean McCarthy <sean.mccarthy@hotmail.com>
2020-08-05 08:38:05 -0600

    changed timing a little bit

--- cron.d/cron_setup.sh
+++ cron.d/cron_setup.sh
@@ -18,7 +18,7 @@ BASH_ENV=/container.env
 # min hour dom month dow   command
 # Delete duplicate alarm log records once daily
 1 1 * * * python3 /cron.d/alarm_log_delete_duplicates.py >> /var/log/cron.log 2>&1
-*/5 * * * * python3 /cron.d/alarm_log_mv_refresh.py >> /var/log/cron.log 2>&1
+*/3 * * * * python3 /cron.d/alarm_log_mv_refresh.py >> /var/log/cron.log 2>&1
 */14 * * * * python3 /cron.d/alarm_log_mv_refresh_old_non_surface.py >> /var/log/cron.log 2>&1
 */2 * * * * python3 /cron.d/time_series_mv_refresh.py >> /var/log/cron.log 2>&1
 */9 * * * * python3 /cron.d/gateways_mv_refresh.py >> /var/log/cron.log 2>&1
