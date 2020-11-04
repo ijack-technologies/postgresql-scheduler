e551891582b3fbcbe44c99baa70ceac386ef685c
Sean McCarthy <sean.mccarthy@hotmail.com>
2020-11-04 10:11:42 -0700

    updates GPS from gateway to public.structures table

--- cron.d/update_gw_power_unit_id_from_shadow.py
+++ cron.d/update_gw_power_unit_id_from_shadow.py
@@ -22,6 +22,34 @@ LOG_LEVEL = logging.INFO
 LOGFILE_NAME = "update_gw_power_unit_id_from_shadow"
 
 
+def convert_to_float(string):
+    try:
+        return float(string)
+    except Exception:
+        c.logger.info(f"Cannot convert '{string}' to float...")
+        return 0
+
+
+def update_structures_table(c, power_unit_id, column, new_value, structure, db_value):
+    sql_update = f"""
+        update public.structures
+        set {column} = {new_value}
+        where power_unit_id = {power_unit_id}
+            -- structure = {structure}
+            -- previous value = {db_value}
+    """
+    run_query(c, sql_update, db="ijack", fetchall=False, commit=True)
+    subject = "Changing GPS in public.structures table!"
+    send_mailgun_email(c, text=sql_update, html='', emailees_list=c.EMAIL_LIST_DEV, subject=subject)
+
+
+def compare_shadow_and_db(shadow_value, db_value, db_column, power_unit_id, structure):
+    shadow_value2 = round(convert_to_float(shadow_value), 2)
+    db_value2 = round(convert_to_float(db_value), 2)
+    if shadow_value2 != 0 and shadow_value2 != db_value2:
+        update_structures_table(c, power_unit_id, db_column, shadow_value, structure, db_value)
+
+
 def main(c):
     """
     Query unit data from AWS RDS "IJACK" PostgreSQL database, 
@@ -34,6 +62,7 @@ def main(c):
     # Get gateway records
     sql_gw = """
         select 
+            distinct on (aws_thing)
             t1.aws_thing, 
             t1.power_unit_id, 
             t2.power_unit
@@ -56,6 +85,26 @@ def main(c):
     _, pu_rows = run_query(c, sql_pu, db="ijack", fetchall=True)
     pu_dict = {row['power_unit']: row['power_unit_id'] for row in pu_rows}
 
+    # Get structure records for comparing GPS lat/lon by power unit
+    sql_structures = """
+        select 
+            structure,
+            power_unit_id,
+            gps_lat,
+            gps_lon
+        from public.structures
+        where power_unit_id is not null
+    """
+    _, structure_rows = run_query(c, sql_structures, db="ijack", fetchall=True)
+    # structures_dict = {
+    #     row['power_unit']: {
+    #         row['structure_id']
+    #         row['power_unit_id'], 
+    #     }
+
+    #     for row in structure_rows
+    # }
+
     # df = pd.DataFrame(gw_rows, columns=columns)
 
     # Get the Boto3 AWS IoT client for updating the "thing shadow"
@@ -75,6 +124,9 @@ def main(c):
         shadow = get_iot_device_shadow(c, client_iot, aws_thing)
         reported = shadow.get('state', {}).get('reported', {})
         power_unit_shadow = reported.get('SERIAL_NUMBER', None)
+        latitude_shadow = reported.get('LATITUDE', None)
+        longitude_shadow = reported.get('LONGITUDE', None)
+
         if power_unit_shadow is None:
             c.logger.warning(f'Power unit "SERIAL_NUMBER" not in shadow for aws_thing "{aws_thing}". Continuing with next AWS_THING in public.gw table...')
             continue
@@ -94,6 +146,13 @@ Continuing with next AWS_THING in public.gw table...")
 
         # Get the power_unit_id already in the public.gw table
         power_unit_id_gw = dict_.get('power_unit_id', None)
+
+        # Compare the GPS first
+        structure_rows_relevant = [row for row in structure_rows if row['power_unit_id'] == power_unit_id_gw]
+        for row in structure_rows_relevant:
+            structure = row['structure']
+            compare_shadow_and_db(latitude_shadow, row['gps_lat'], 'gps_lat', power_unit_id_gw, structure)
+            compare_shadow_and_db(longitude_shadow, row['gps_lon'], 'gps_lon', power_unit_id_gw, structure)
 
         if power_unit_id_shadow == power_unit_id_gw:
             c.logger.info(f"Power unit '{power_unit_shadow}' in the public.gw table matches the one reported in the device shadow. Continuing with next...")
