f17e8e57fa30cc6a57c6c0aaab6465e743a8b9fd
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-03-02 11:20:51 -0700

    commenting out this old module we no longer use

--- cron.d/gateways_mv_refresh.py
+++ cron.d/gateways_mv_refresh.py
@@ -1,44 +1,44 @@
-import logging
-import pathlib
+# import logging
+# import pathlib
 
-# local imports
-from utils import (
-    Config,
-    configure_logging,
-    error_wrapper,
-    exit_if_already_running,
-    run_query,
-)
+# # local imports
+# from utils import (
+#     Config,
+#     configure_logging,
+#     error_wrapper,
+#     exit_if_already_running,
+#     run_query,
+# )
 
-LOG_LEVEL = logging.INFO
-LOGFILE_NAME = "gateways_mv_refresh"
-
-
-# Requires owner privileges (must be run by "master" user, not "app_user")
-# Note this one does NOT run concurrently because it doesn't have a unique index.
-# Some gateways serve two structures (e.g. dual XFERs), so they're duplicates re: power_unit field
-SQL = """
-    REFRESH MATERIALIZED VIEW 
-    public.gateways
-    WITH DATA
-"""
+# LOG_LEVEL = logging.INFO
+# LOGFILE_NAME = "gateways_mv_refresh"
 
 
-@error_wrapper()
-def main(c):
-    """Main entrypoint function"""
-    global SQL
-
-    exit_if_already_running(c, pathlib.Path(__file__).name)
-
-    run_query(c, SQL, commit=True)
-
-    return None
+# # Requires owner privileges (must be run by "master" user, not "app_user")
+# # Note this one does NOT run concurrently because it doesn't have a unique index.
+# # Some gateways serve two structures (e.g. dual XFERs), so they're duplicates re: power_unit field
+# SQL = """
+#     REFRESH MATERIALIZED VIEW 
+#     public.gateways
+#     WITH DATA
+# """
 
 
-if __name__ == "__main__":
-    c = Config()
-    c.logger = configure_logging(
-        __name__, logfile_name=LOGFILE_NAME, path_to_log_directory="/var/log/"
-    )
-    main(c)
+# @error_wrapper()
+# def main(c):
+#     """Main entrypoint function"""
+#     global SQL
+
+#     exit_if_already_running(c, pathlib.Path(__file__).name)
+
+#     run_query(c, SQL, commit=True)
+
+#     return None
+
+
+# if __name__ == "__main__":
+#     c = Config()
+#     c.logger = configure_logging(
+#         __name__, logfile_name=LOGFILE_NAME, path_to_log_directory="/var/log/"
+#     )
+#     main(c)
