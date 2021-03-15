383f53bd4d3c6fd92085b05a3d825a251ac4cfe1
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-03-15 08:46:36 -0600

    don't delete old connections since it's causing errors

--- cron.d/cron_setup.sh
+++ cron.d/cron_setup.sh
@@ -17,7 +17,7 @@ BASH_ENV=/container.env
 
 # min hour dom month dow   command
 # Delete duplicate alarm log records once daily
-*/15 * * * * python3 /cron.d/db_remove_old_connections.py
+# */15 * * * * python3 /cron.d/db_remove_old_connections.py
 1 1 * * * python3 /cron.d/alarm_log_delete_duplicates.py
 */1 * * * * python3 /cron.d/alarm_log_mv_refresh.py
 */4 * * * * python3 /cron.d/time_series_mv_refresh.py
