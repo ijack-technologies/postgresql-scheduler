e9614d6c5fd720508316aeb8f520cf3608b433ea
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-05-14 15:50:48 -0600

    only refresh the LOCF materialized view every 30 mins, and don't refresh the hybrid timespan mat. view anymore

--- cron.d/cron_setup.sh
+++ cron.d/cron_setup.sh
@@ -20,7 +20,7 @@ BASH_ENV=/container.env
 # */15 * * * * python3 /cron.d/db_remove_old_connections.py
 1 1 * * * python3 /cron.d/alarm_log_delete_duplicates.py
 */1 * * * * python3 /cron.d/alarm_log_mv_refresh.py
-*/4 * * * * python3 /cron.d/time_series_mv_refresh.py
+*/30 * * * * python3 /cron.d/time_series_mv_refresh.py
 # */3 * * * * python3 /cron.d/gateways_mv_refresh.py
 3 * * * * python3 /cron.d/synch_aws_iot_shadow_with_aws_rds_postgres_config.py
 15 * * * * python3 /cron.d/update_gw_power_unit_id_from_shadow.py
