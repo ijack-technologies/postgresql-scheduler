f108adb8c44147cda78288606dd40c51f02726bd
Sean McCarthy <sean.mccarthy@hotmail.com>
2022-02-09 21:54:30 -0700

    new slave unit metrics for postgreSQL inserter

--- cron_d/time_series_mv_refresh.py
+++ cron_d/time_series_mv_refresh.py
@@ -88,7 +88,18 @@ def get_and_insert_latest_values(c):
         stroke_up_time, stroke_down_time,
         HYD_OIL_LVL, HYD_FILT_LIFE, HYD_OIL_LIFE,
         -- booleans below
-		hyd, hyd_egas, warn1, warn1_egas, warn2, warn2_egas, mtr, mtr_egas, clr, clr_egas, htr, htr_egas, aux_egas, prs, sbf
+		hyd, hyd_egas, warn1, warn1_egas, warn2, warn2_egas, mtr, mtr_egas, clr, clr_egas, htr, htr_egas, aux_egas, prs, sbf,
+        -- new slave metrics
+        STROKE_UP_TIME_SL,
+        STROKE_DOWN_TIME_SL,
+        SPM_EGAS_SL,
+        STROKE_SPEED_AVG_SL,
+        CGP_SL,
+        DGP_SL,
+        AGFN_SL,
+        AGFM_SL,
+        HP_RAISING_AVG_SL,
+        HP_LOWERING_AVG_SL
     )
     select
         timestamp_utc, gateway,
@@ -102,7 +113,18 @@ def get_and_insert_latest_values(c):
         stroke_up_time, stroke_down_time,
         HYD_OIL_LVL, HYD_FILT_LIFE, HYD_OIL_LIFE,
         -- booleans below
-		hyd, hyd_egas, warn1, warn1_egas, warn2, warn2_egas, mtr, mtr_egas, clr, clr_egas, htr, htr_egas, aux_egas, prs, sbf
+		hyd, hyd_egas, warn1, warn1_egas, warn2, warn2_egas, mtr, mtr_egas, clr, clr_egas, htr, htr_egas, aux_egas, prs, sbf,
+        -- new slave metrics
+        STROKE_UP_TIME_SL,
+        STROKE_DOWN_TIME_SL,
+        SPM_EGAS_SL,
+        STROKE_SPEED_AVG_SL,
+        CGP_SL,
+        DGP_SL,
+        AGFN_SL,
+        AGFM_SL,
+        HP_RAISING_AVG_SL,
+        HP_LOWERING_AVG_SL
 	FROM public.time_series_locf
     --Use the "latest_ts_utc" table from above as a filter
     where timestamp_utc > (select timestamp_utc from latest_ts_utc)
