f5a837d59c34c4e4b0b26977e9f76ce7b397d00d
Sean McCarthy <sean.mccarthy@hotmail.com>
2024-04-17 14:55:56 -0600

    make update way faster in threadpool executor

--- cron_d/synch_aws_iot_shadow_with_aws_rds_postgres_config.py
+++ cron_d/synch_aws_iot_shadow_with_aws_rds_postgres_config.py
@@ -5,6 +5,10 @@ import pathlib
 import sys
 import time
 from decimal import Decimal
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import boto3
+from botocore.response import StreamingBody
+from botocore.exceptions import ClientError
 
 # Insert pythonpath into the front of the PATH environment variable, before importing anything from canpy
 pythonpath = str(pathlib.Path(__file__).parent.parent)
@@ -31,8 +35,78 @@ c.logger = configure_logging(
 )
 
 
+def update_device_shadows_in_threadpool(
+    c: Config, gateways_to_update: dict, client_iot: boto3.client
+) -> list:
+    """Use concurrent.futures.ThreadPoolExecutor to efficiently gather all AWS IoT device shadows"""
+
+    max_workers = 20
+    n_gateways = len(gateways_to_update)
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        c.logger.info(
+            f"Updating {n_gateways} gateways' AWS IoT device shadows in thread pool..."
+        )
+
+        future_to_aws_thing_dict = {}
+        for aws_thing, json_payload_str in gateways_to_update.items():
+            future_to_aws_thing_dict[
+                executor.submit(
+                    client_iot.update_thing_shadow,
+                    thingName=aws_thing,
+                    payload=json_payload_str,
+                )
+            ] = aws_thing
+
+        time1 = time.time()
+        success_dict = {}
+        errors_dict = {}
+        for index, future in enumerate(as_completed(future_to_aws_thing_dict)):
+            c.logger.info(f"{index + 1} of {n_gateways} shadows updated")
+            status_code: int = None
+            response_payload: str = None
+            try:
+                aws_thing = future_to_aws_thing_dict[future]
+                response: dict = future.result()
+                streamingBody: StreamingBody = response["payload"]
+                response_payload = json.loads(streamingBody.read())
+                # HTTP status code
+                status_code = response.get("ResponseMetadata", {}).get(
+                    "HTTPStatusCode", None
+                )
+            except ClientError as err:
+                assert err.response["Error"]["Code"] == "ResourceNotFoundException"
+                status_code = 404
+                response_payload = "ResourceNotFoundException"
+                errors_dict[aws_thing] = response_payload
+            except Exception as exc:
+                status_code = 500
+                response_payload = str(type(exc))
+                errors_dict[aws_thing] = response_payload
+                raise
+            else:
+                success_dict[aws_thing] = {
+                    "status": status_code,
+                    "payload": response_payload,
+                }
+                # print(str(len(success_dict)), end="\r")
+
+        time2 = time.time()
+
+    n_success_dict = len(success_dict)
+    c.logger.info(
+        f"{n_success_dict} of {n_gateways} AWS IoT shadows successfully updated in {(time2-time1)/60:.1f} minutes!"
+    )
+    for aws_thing, response_payload in errors_dict.items():
+        c.logger.error(
+            f"Error updating AWS IoT shadow for {aws_thing}: {response_payload}"
+        )
+
+    return success_dict
+
+
 @error_wrapper()
-def main(c):
+def main(c: Config) -> None:
     """
     Query unit data from AWS RDS "IJACK" PostgreSQL database,
     and update it in the AWS IoT "thing shadow" from which the gateways
@@ -46,23 +120,42 @@ def main(c):
     # df = pd.DataFrame(rows, columns=columns)
 
     # Get the Boto3 AWS IoT client for updating the "thing shadow"
-    client_iot = get_client_iot()
+    client_iot: boto3.client = get_client_iot()
 
     # Get all gateways from database, and all the fields we're going
     # to update in the AWS IoT device shadow with C__{METRIC}
     rows: list = get_all_gateways_config_metrics(c)
 
+    # Dict to which we'll add aws_thing: shadow pairs,
+    # which we'll then update efficiently in a thread pool
+    gateways_to_update: dict = {}
+
     n_rows = len(rows)
     time_start = time.time()
-    for counter, dict_ in enumerate(rows):
+    for dict_ in rows:
+        # Logger info
+        aws_thing = dict_["aws_thing"].upper()
+
+        # customer = None
+        # try:
+        #     # Get a slightly shorter customer name, if available
+        #     customer = str(dict_["mqtt_topic"]).title()
+        # except Exception:
+        #     c.logger.exception(
+        #         "Trouble finding the MQTT topic. Is this the SHOP gateway? Continuing with the customer name instead..."
+        #     )
+        #     customer = dict_["customer"]
+        # c.logger.info(
+        #     f"Preparing {counter + 1} of {n_rows} for {customer} AWS_THING: {aws_thing}..."
+        # )
+
         # Initialize a new thing shadow for the data we're going to update in AWS IoT
-        d = {"state": {"reported": {}}}
+        shadow_new = {"state": {"reported": {}}}
 
         # if dict_["gateway"] == "00:60:E0:84:A6:C7":
         #     # Just for debugging. Comment out if you don't need this
         #     print("found it")
 
-        aws_thing = dict_["aws_thing"].upper()
         for key, value in dict_.items():
             # For debugging
             # if key == "ip_modbus" and aws_thing == "00:60:E0:84:A6:DB":
@@ -76,41 +169,33 @@ def main(c):
             if isinstance(value, Decimal):
                 value = float(value)
             if key in ("gateway", "unit_type", "aws_thing"):
-                d["state"]["reported"][f"C__{key.upper()}"] = value.upper()
+                shadow_new["state"]["reported"][f"C__{key.upper()}"] = value.upper()
             else:
-                d["state"]["reported"][f"C__{key.upper()}"] = value
+                shadow_new["state"]["reported"][f"C__{key.upper()}"] = value
 
-        # Logger info
-        customer = None
         try:
-            # Get a slightly shorter customer name, if available
-            customer = str(dict_["mqtt_topic"]).title()
-        except Exception:
-            c.logger.exception(
-                "Trouble finding the MQTT topic. Is this the SHOP gateway? Continuing with the customer name instead..."
-            )
-            customer = dict_["customer"]
+            json_payload_str: str = json.dumps(shadow_new)
+            gateways_to_update[aws_thing] = json_payload_str
 
-        c.logger.info(
-            f"{counter + 1} of {n_rows}: Updating {customer} AWS_THING: {aws_thing}"
-        )
-
-        # Update the thing shadow for this gateway/AWS_THING
-        try:
-            json_payload_str: str = json.dumps(d)
-            client_iot.update_thing_shadow(
-                thingName=aws_thing, payload=json_payload_str
-            )
+            # Update the thing shadow for this gateway/AWS_THING
+            # c.logger.info(
+            #     f"{counter + 1} of {n_rows}: Updating {customer} AWS_THING: {aws_thing}"
+            # )
+            # client_iot.update_thing_shadow(
+            #     thingName=aws_thing, payload=json_payload_str
+            # )
         except TypeError:
             # If there's a problem with the JSON serialization, log the error and stop the program!
             c.logger.exception(
-                "ERROR updating AWS IoT shadow for aws_thing '%s'", aws_thing
+                "ERROR serializing JSON string for aws_thing '%s'", aws_thing
             )
             raise
-        except Exception:
-            c.logger.exception(
-                "ERROR updating AWS IoT shadow for aws_thing '%s'", aws_thing
-            )
+        # except Exception:
+        #     c.logger.exception(
+        #         "ERROR updating AWS IoT shadow for aws_thing '%s'", aws_thing
+        #     )
+
+    update_device_shadows_in_threadpool(c, gateways_to_update, client_iot)
 
     time_finish = time.time()
     c.logger.info(
