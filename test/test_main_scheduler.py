a07fc6da683e8537c333953606146bf7f1e2ab58
Sean McCarthy <sean.mccarthy@hotmail.com>
2024-08-22 14:34:01 -0600

    Update test_main_scheduler.py

--- test/test_main_scheduler.py
+++ test/test_main_scheduler.py
@@ -29,30 +29,31 @@ class TestAll(unittest.TestCase):
     """Test all functions"""
 
     # @patch("time.sleep", return_value=None)
-    @patch("project.time_series_mv_refresh.get_and_insert_latest_values")
-    @patch("project.time_series_mv_refresh.force_refresh_continuous_aggregates")
-    @patch("project.time_series_mv_refresh.check_table_timestamps")
-    @patch("project.time_series_mv_refresh.get_latest_timestamp_in_table")
-    @patch("project.time_series_mv_refresh.exit_if_already_running")
+    # @patch("project.time_series_mv_refresh.get_and_insert_latest_values")
+    # @patch("project.time_series_mv_refresh.force_refresh_continuous_aggregates")
+    # @patch("project.time_series_mv_refresh.check_table_timestamps")
+    # @patch("project.time_series_mv_refresh.get_latest_timestamp_in_table")
+    # @patch("project.time_series_mv_refresh.exit_if_already_running")
     def test_time_series_mv_refresh(
         self,
-        mock_exit_if_already_running,
-        mock_get_latest_timestamp_in_table,
-        mock_check_table_timestamps,
-        mock_force_refresh_continuous_aggregates,
-        mock_get_and_insert_latest_values,
+        # mock_exit_if_already_running,
+        # mock_get_latest_timestamp_in_table,
+        # mock_check_table_timestamps,
+        # mock_force_refresh_continuous_aggregates,
+        # mock_get_and_insert_latest_values,
         # mock_time_sleep
     ):
         """Test the main program"""
         global c
 
-        # def slow_func():
-        #     print("Running slow_func...")
-        #     time.sleep(10)
+        c.test_var = False
 
-        # schedule.every(1).seconds.do(slow_func)
+        def slow_func(c: Config) -> None:
+            """Sets test_var to True"""
+            c.logger.info("Slow function running...")
+            c.test_var = True
 
-        schedule.every(1).seconds.do(time_series_mv_refresh.main, c)
+        schedule.every(1).seconds.do(slow_func, c=c)
 
         time_start = time.time()
         time_finish = time.time() + 0.01
@@ -65,11 +66,12 @@ class TestAll(unittest.TestCase):
         seconds_taken = time.time() - time_start
         print(f"Seconds taken: {seconds_taken}")
 
-        self.assertTrue(mock_exit_if_already_running.call_count > 0)
-        self.assertTrue(mock_get_latest_timestamp_in_table.call_count > 0)
-        self.assertTrue(mock_check_table_timestamps.call_count > 0)
-        self.assertTrue(mock_force_refresh_continuous_aggregates.call_count > 0)
-        self.assertTrue(mock_get_and_insert_latest_values.call_count > 0)
+        self.assertTrue(c.test_var)
+        # self.assertTrue(mock_exit_if_already_running.call_count > 0)
+        # self.assertTrue(mock_get_latest_timestamp_in_table.call_count > 0)
+        # self.assertTrue(mock_check_table_timestamps.call_count > 0)
+        # self.assertTrue(mock_force_refresh_continuous_aggregates.call_count > 0)
+        # self.assertTrue(mock_get_and_insert_latest_values.call_count > 0)
 
         # mock_time_sleep.assert_called()
 
