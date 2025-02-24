137cdda72e65e2e4605b3db4af42a5de13524e03
Sean McCarthy <sean.mccarthy@hotmail.com>
2025-02-24 14:26:20 -0700

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# First setup the cron job to run some database refresh jobs
-# e.g. alarm log materialized view
-/bin/bash /project/cron_setup.sh
-
-# Ah, ha, ha, ha, stayin' alive...
-# https://github.com/docker/compose/issues/1926
-while :; do :; done & kill -STOP $! && wait $!
