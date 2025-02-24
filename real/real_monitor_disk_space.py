32678fb0dce0a27c372b146808c769112bf9f391
Sean McCarthy <sean.mccarthy@hotmail.com>
2025-02-24 11:24:11 -0700

    Create real_monitor_disk_space.py

---
+++ real/real_monitor_disk_space.py
@@ -0,0 +1,53 @@
+# # Load the secret environment variables using python-dotenv
+# from dotenv import load_dotenv
+# load_dotenv()
+
+import sys
+import unittest
+
+# Insert pythonpath into the front of the PATH environment variable, before importing anything from project/
+pythonpath = "/workspace"
+try:
+    sys.path.index(pythonpath)
+except ValueError:
+    sys.path.insert(0, pythonpath)
+
+
+from project import (
+    monitor_disk_space,
+)
+from project.utils import Config
+
+# Insert pythonpath into the front of the PATH environment variable, before importing anything from project/
+pythonpath = "/workspace"
+try:
+    sys.path.index(pythonpath)
+except ValueError:
+    sys.path.insert(0, pythonpath)
+
+
+LOGFILE_NAME = "test_main_programs"
+
+c = Config()
+c.DEV_TEST_PRD = "development"
+
+
+class TestAll(unittest.TestCase):
+    # # executed after each test
+    # def tearDown(self):
+    #     pass
+
+    # executed prior to each test below, not just when the class is initialized
+    def setUp(self):
+        global c
+        c.DEV_TEST_PRD = "development"
+        c.TEST_FUNC = True
+
+    def test_monitor_disk_space(self):
+        """Test the main program"""
+        global c
+        monitor_disk_space.main(c)
+
+
+if __name__ == "__main__":
+    unittest.main()
