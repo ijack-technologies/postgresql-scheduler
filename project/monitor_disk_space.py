50dc17bb5200e7972f2ae5337fdfce2fa9ddbe56
Sean McCarthy <sean.mccarthy@hotmail.com>
2025-02-24 10:59:06 -0700

    Insert pythonpath into the front of the PATH environment variable, before importing anything from project/

--- project/monitor_disk_space.py
+++ project/monitor_disk_space.py
@@ -1,5 +1,13 @@
 import subprocess
+import sys
 from pathlib import Path
+
+# Insert pythonpath into the front of the PATH environment variable, before importing anything from project/
+pythonpath = str(Path(__file__).parent.parent)
+try:
+    sys.path.index(pythonpath)
+except ValueError:
+    sys.path.insert(0, pythonpath)
 
 from project.logger_config import logger
 from project.utils import (
