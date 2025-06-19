10f0c0971038abfec863781a343a602effc18a8b
Sean McCarthy <sean.mccarthy@hotmail.com>
2025-06-19 19:42:49 +0000

    Added starter for FX exchange rates program

---
+++ real/real_update_fx_exchange_rates_daily.py
@@ -0,0 +1,44 @@
+# # Load the secret environment variables using python-dotenv
+# from dotenv import load_dotenv
+# load_dotenv()
+
+import sys
+import unittest
+from unittest.mock import patch
+
+# Insert pythonpath into the front of the PATH environment variable, before importing anything from project/
+pythonpath = "/workspace"
+try:
+    sys.path.index(pythonpath)
+except ValueError:
+    sys.path.insert(0, pythonpath)
+
+
+from project import update_fx_exchange_rates_daily
+from project.utils import Config
+
+c = Config()
+c.DEV_TEST_PRD = "development"
+
+
+class TestAll(unittest.TestCase):
+    # # executed after each test
+    # def tearDown(self):
+    #     pass
+
+    # executed prior to each test below, not just when the class is initialized
+    def setUp(self):
+        global c
+        c.DEV_TEST_PRD = "development"
+        # This c.TEST_FUNC just disables SMS, email, and phone call alerts
+        c.TEST_FUNC = False
+
+    @patch("project.update_fx_exchange_rates_daily.exit_if_already_running")
+    def test_update_fx_exchange_rates_daily(self, mock_exit_if_already_running):
+        """Test the main program"""
+        global c
+        update_fx_exchange_rates_daily.main(c)
+
+
+if __name__ == "__main__":
+    unittest.main()
